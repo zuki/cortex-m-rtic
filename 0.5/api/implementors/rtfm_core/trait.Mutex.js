ba730651d3f919b47f4328b0750bcfa97ad7b9d0
korken89 <korken89@users.noreply.github.com>
2020-06-11 17:39:18 +0000

    deploy: 6e233bec029810716c5a48113b22f4446204cfab

--- 0.5/api/implementors/rtfm_core/trait.Mutex.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["rtfm_core"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
