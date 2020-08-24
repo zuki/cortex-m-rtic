4d2f99c196e059196386aa117a7f1ee3004334c1
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-25 08:25:53 +0900

    example/smallest.rsをstm32f4対応

--- examples/smallest.rs
+++ examples/smallest.rs
@@ -3,8 +3,10 @@
 #![no_main]
 #![no_std]
 
+use stm32f4::stm32f407;
+
 use panic_semihosting as _; // panic handler
 use rtic::app;
 
-#[app(device = lm3s6965)]
+#[app(device = stm32f407)]
 const APP: () = {};
